89a5dacdbcf64772e621e4bde42ac128ba42a03f
jerik2k3 <118814858+jerik2k3@users.noreply.github.com>
2024-01-30 03:01:58 +0000

    Add OpenAlex API

---
+++ pages/5_OpenAlex_API.py
@@ -0,0 +1,43 @@
+import streamlit as st
+from st_aggrid import AgGrid, GridOptionsBuilder
+import pandas as pd
+import sqlalchemy
+
+engine = sqlalchemy.create_engine("mssql+pyodbc://admin:JYADzncw132MnajClFrF@crawlerdatabase.c61596xtihb7.ap-southeast-1.rds.amazonaws.com/nlpresults?driver=ODBC+Driver+17+for+SQL+Server", fast_executemany=True, connect_args={'charset':'utf8', 'convert_unicode': True})
+df = pd.read_sql_table('grammar', engine)
+#df = pd.read_csv('https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv')
+
+# Number of rows per page
+rows_per_page = 100
+
+# Define the grid options
+grid_options = GridOptionsBuilder.from_dataframe(df)
+grid_options.configure_pagination(paginationPageSize=rows_per_page)
+grid_options.configure_side_bar()
+
+st.title('OpenAlex API')
+
+# Custom CSS to set width to 100%
+st.markdown(
+    """
+    <style>
+    .ag-theme-alpine {
+        width: 100%;        
+    }
+    .st-emotion-cache-1y4p8pa {
+        width: 100%;
+        padding: 6rem 1rem 10rem;
+        max-width: 100% !important;        
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+AgGrid(df,
+    gridOptions=grid_options.build(),    
+    custom_css={
+        "#gridToolBar": {
+            "padding-bottom": "0px !important",
+        }
+    })
