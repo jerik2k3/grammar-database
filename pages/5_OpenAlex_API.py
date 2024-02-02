5a8f1822a5d000034935eb37bf9ad396ae489dd0
jerik2k3 <118814858+jerik2k3@users.noreply.github.com>
2024-02-02 10:43:06 +0000

    optimize

--- pages/5_OpenAlex_API.py
+++ pages/5_OpenAlex_API.py
@@ -4,17 +4,17 @@ import pandas as pd
 import sqlalchemy
 import requests
 
-engine = sqlalchemy.create_engine("mssql+pyodbc://admin:JYADzncw132MnajClFrF@crawlerdatabase.c61596xtihb7.ap-southeast-1.rds.amazonaws.com/nlpresults?driver=ODBC+Driver+17+for+SQL+Server", fast_executemany=True, connect_args={'charset':'utf8', 'convert_unicode': True})
-df = pd.read_sql_table('grammar', engine)
+##engine = sqlalchemy.create_engine("mssql+pyodbc://admin:JYADzncw132MnajClFrF@crawlerdatabase.c61596xtihb7.ap-southeast-1.rds.amazonaws.com/nlpresults?driver=ODBC+Driver+17+for+SQL+Server", fast_executemany=True, connect_args={'charset':'utf8', 'convert_unicode': True})
+##df = pd.read_sql_table('grammar', engine)
 #df = pd.read_csv('https://raw.githubusercontent.com/fivethirtyeight/data/master/airline-safety/airline-safety.csv')
 
 # Number of rows per page
-rows_per_page = 100
+rows_per_page = 25
 
 # Define the grid options
-grid_options = GridOptionsBuilder.from_dataframe(df)
-grid_options.configure_pagination(paginationPageSize=rows_per_page)
-grid_options.configure_side_bar()
+##grid_options = GridOptionsBuilder.from_dataframe(df)
+##grid_options.configure_pagination(paginationPageSize=rows_per_page)
+##grid_options.configure_side_bar()
 
 st.title('OpenAlex API')
 
